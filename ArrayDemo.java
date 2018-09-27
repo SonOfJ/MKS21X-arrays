a86a9bbebd4ebb7474882464437ed56c4778031a
Jason Zheng <zzheng3@stuy.edu>
2018-09-27 02:40:30 -0400

    needed to use System.out.println to get a value

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -4,11 +4,11 @@ System.out.println("0a.");
     int[] ary = {1, 2, 3, 4};
     printArray(ary);
 System.out.println("0b.");
-   int[][] ary2 = new int[][] {
-      {1, 2, 3, 4},
-      {4, 5, 6, 7},
-    };
+   int[][] ary2 = new int[][] {{1, 2, 3, 4},{4, 5, 6, 7}};
     printArray(ary2);
+System.out.println("1.");
+   int[][] ary3 = new int[][] {{0, 2, 3, 0},{4, 0, 0, 7}};
+    System.out.println(countZeros2D(ary3));
 }
   public static void printArray (int[] ary) {
     for (int i = 0; i < ary.length; i = i + 1) {
@@ -19,9 +19,21 @@ System.out.println("0b.");
       }
     }
   }
-  public static void printArray(int[][] ary){
+  public static void printArray(int[][] ary) {
     for (int i = 0; i < ary.length; i = i + 1) {
       printArray(ary[i]);
     }
+  }
+  public static int countZeros2D(int[][] nums) {
+    int count = 0;
+    int i = 0;
+    for (; i < nums.length; i = i + 1) {
+      for (int k = 0; k < nums[i].length; k = k + 1) {
+        if (nums[i][k] == 0) {
+          count = count + 1;
+        }
+      }
+    }
+    return count;
   }
 }
