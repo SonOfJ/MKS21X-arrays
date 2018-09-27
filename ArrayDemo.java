644a1e28b25da02c598ebbf142905003364847b3
Jason Zheng <zzheng3@stuy.edu>
2018-09-27 03:01:41 -0400

    added printArray so that I would actually get a result

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -9,6 +9,9 @@ System.out.println("0b.");
 System.out.println("1.");
    int[][] ary3 = new int[][] {{0, 2, 3, 0},{4, 0, 0, 7}};
     System.out.println(countZeros2D(ary3));
+System.out.println("2a.");
+   int[][] ary4 = new int[][] {{3, 5, 3, 6},{4, 7, 5, 1},{8, 4, 3, 6}};
+    fill2D(ary4);
 }
   public static void printArray (int[] ary) {
     for (int i = 0; i < ary.length; i = i + 1) {
@@ -35,5 +38,17 @@ System.out.println("1.");
       }
     }
     return count;
+  }
+  public static void fill2D(int[][] vals) {
+    for (int i = 0; i < vals.length; i = i + 1) {
+      for (int k = 0; k < vals[i].length; k = k + 1) {
+        if (i == k) {
+          vals[i][k] = 3;
+        } else {
+          vals[i][k] = 1;
+        }
+      }
+    }
+    printArray(vals);
   }
 }
